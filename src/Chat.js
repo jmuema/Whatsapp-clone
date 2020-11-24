c9c7f6e7fac7274d56fdb54e164eda03ca254635
jmuema <jmuema95@gmail.com>
2020-11-24 09:50:20 +0300

    creating the chat message section

--- src/Chat.js
+++ src/Chat.js
@@ -1,14 +1,48 @@
-import { Avatar } from '@material-ui/core';
-import React from 'react';
-import "./Chat.css"
+import { Avatar, IconButton } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import "./Chat.css";
+import SearchOutlined from '@material-ui/icons/SearchOutlined';
+import  AttachFile from '@material-ui/icons/AttachFile';
+import MoreVert from '@material-ui/icons/MoreVert';
+import { Message } from '@material-ui/icons';
+
+
+
 
 function Chat() {
+    const [seed, setSeed] = useState("");
+
+    useEffect (()=> {
+        setSeed(Math.floor(Math.random() * 5000));
+    }, []);
+
     return (
         <div className="chat" >
             <div className="chat__header">
-                <Avatar/>
+            <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />  
+            <div className="chat__headerInfo">
+                <h3> Room name</h3>
+                <p> Last seen at ...</p>
+                </div> 
+                <div className="chat__headerRight">
+                <IconButton> 
+                <SearchOutlined/>
+                     </IconButton>
+                     <IconButton> 
+                         <AttachFile/>
+                    
+                    </IconButton>
+                    <IconButton> 
+                        <MoreVert/>
+                    
+                    </IconButton>
+
+                </div>
             </div>
             <div className="chat__body">
+                <p className="chat__message" > Hey Guys  
+    <span className="chat__name" > Joseph Muema</span>
+                </p>
 
             </div>
             <div className="chat__footer">
