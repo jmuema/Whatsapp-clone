322b2810627068df683e3720c6817b66c5375e9f
jmuema <jmuema95@gmail.com>
2020-12-09 22:08:54 +0300

    done

--- src/Chat.js
+++ src/Chat.js
@@ -4,9 +4,14 @@ import "./Chat.css";
 import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
 import MicIcon from "@material-ui/icons/Mic";
 import SearchOutlined from '@material-ui/icons/SearchOutlined';
-import  AttachFile from '@material-ui/icons/AttachFile';
+import AttachFile from '@material-ui/icons/AttachFile';
 import MoreVert from '@material-ui/icons/MoreVert';
 import { useParams } from 'react-router-dom';
+import db from "./firebase";
+import firebase from "firebase";
+import { useStateValue } from "./StateProvider";
+
+
 // import { Message } from '@material-ui/icons';
 
 
@@ -16,14 +21,43 @@ function Chat() {
     const [input, setInput] = useState("");
     const [seed, setSeed] = useState("");
     const { roomId } = useParams();
+    const [roomName, setRoomName] = useState("");
+    const [messages, setMessages] = useState([]);
+    const [{user}, dispatch] = useStateValue();
 
-    useEffect (()=> {
+    useEffect(() => {
+        if (roomId) {
+            db.collection("rooms")
+            .doc(roomId)
+            .onSnapshot((snapshot) => setRoomName
+            (snapshot.data().name));
+
+            db.collection("rooms")
+            .doc(roomId)
+            .collection("messages")
+            .orderBy("timestamp", "asc")
+            .onSnapshot((snapshot) => 
+              setMessages(snapshot.docs.map((doc) => 
+              doc.data()))
+            );
+
+        }
+
+    }, [roomId]);
+
+    useEffect(() => {
         setSeed(Math.floor(Math.random() * 5000));
     }, []);
-    
+
     const sendMessage = (e) => {
         e.preventDefault();
         console.log('you typed >>>', input);
+
+        db.collection('rooms').doc(roomId).collection('messages').add({
+            message: input,
+            name: user.displayName,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        })
 
         setInput("");
 
@@ -32,44 +66,50 @@ function Chat() {
     return (
         <div className="chat" >
             <div className="chat__header">
-            <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />  
-            <div className="chat__headerInfo">
-                <h3> Room name</h3>
-                <p> Last seen at ...</p>
-                </div> 
+                <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+                <div className="chat__headerInfo">
+                    <h3>{roomName}</h3>
+                    <p> Last seen {" "}
+                    {new Date(messages[messages.length - 1]?.timestamp?.toDate()).toUTCString()}
+                    </p>
+                </div>
                 <div className="chat__headerRight">
-                <IconButton> 
-                <SearchOutlined/>
-                     </IconButton>
-                     <IconButton> 
-                         <AttachFile/>
-                    
+                    <IconButton>
+                        <SearchOutlined />
                     </IconButton>
-                    <IconButton> 
-                        <MoreVert/>
-                    
+                    <IconButton>
+                        <AttachFile />
+
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert />
+
                     </IconButton>
 
                 </div>
             </div>
             <div className="chat__body">
-                <p className={`chat__message ${true && 'chat__reciever'}`} > 
-                <span className="chat__name" > Joseph Muema</span>
-                Hey Guys
-                <span className="chat__timestamp" > 3:52pm</span>
+                {messages.map((message) => (
+                      <p className={`chat__message ${ message.name === user.displayName && 'chat__reciever'}`} >
+                      <span className="chat__name" > {message.name}</span>
+                      {message.message}
+                <span className="chat__timestamp" >{new Date(message.timestamp?.toDate()).toUTCString()} </span>
   
-                </p>
+                  </p>
+
+                ))}
+              
 
             </div>
             <div className="chat__footer">
-                <InsertEmoticonIcon/>
+                <InsertEmoticonIcon />
                 <form>
-                    <input value={input} onChange={e => setInput (e.target.value)} placeholder="Type a message" type="text"/>
+                    <input value={input} onChange={e => setInput(e.target.value)} placeholder="Type a message" type="text" />
                     <button onClick={sendMessage} type="submit"> Send a message </button>
                 </form>
-                <MicIcon/>
+                <MicIcon />
             </div>
-            
+
         </div>
     )
 }
